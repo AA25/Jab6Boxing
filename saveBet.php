4ae1fb3f0dbb85f67797f4eb52a134e2c7e30393
Jake Tongue <jto01@sbgml03150.sbg.skybettingandgaming.com>
2017-09-22 15:12:40 +0100

    Submission into database

--- saveBet.php
+++ saveBet.php
@@ -1,39 +1,53 @@
 <?php
 include('includes/sessions.inc.php');
+include('includes/sqlConnect.inc.php');
+
+spl_autoload_register(function($className){
+  $className = strtolower($className);
+  require __DIR__."/classes/$className.php";
+});
+
 
 date_default_timezone_set('Europe/London');
-$timeOfBet = time();
-$recoveredBetBy = file_get_contents('betBy.txt');
-$betByTime = unserialize($recoveredBetBy);
 
-$recoveredBets = file_get_contents('bets.txt');
-$bets = unserialize($recoveredBets);
+$time = explode(" ",date('Y-m-d H:i:s'));
+$currentDate = $time[0];
+$currentTime = $time[1];
+$userId = "";
 
-if($timeOfBet < $betByTime){
+if(isset($_SESSION['user'])){
+  $user = unserialize (serialize ($_SESSION['user']));
+  $result = $user->getId();
+  foreach($result as $i){
+    $userId = $i['userId'];
+    break;
+  }
+  
+  $p = $pdo->prepare("insert into userPrediction (userId, eventId, tiebreakerPrediction, date, time)
+  values (:userId, :eventId, :tiebreakerPrediction, :date, :time)");
 
-  echo "Your bet has been saved!";
-  ?>
-  <p>Click <a href="checkResults.php">here</a> to check the results</p>
-  <?php
+  $p->execute([
+    'userId' => $userId,
+    'eventId' => $_SESSION['currentEvent'],
+    'tiebreakerPrediction' => $_POST['goldenGlove'],
+    'date' => $currentDate,
+    'time' => $currentTime
+  ]);
 
-  $prediction = array(
-    $_SESSION['username'],
-    array($_POST['match1'], $_POST['match1Round']),
-    array($_POST['match2'], $_POST['match2Round']),
-    array($_POST['match3'], $_POST['match3Round']),
-    $_POST['goldenGlove']
-  );
+  $predictionId = $pdo->lastInsertId();
+  for($i=1; $i<7; $i++){
+    $round = $_POST["match".$i."Round"];
+    $name = "match".$i;
+    $fighter = $_POST[$name];
+    $m = $pdo->prepare("insert into userMatchPredictions (predictionId, matchId, predictedResult, predictedRound)
+    values (:predictionId, :matchId, :predictedResult, :predictedRound)");
 
-  array_push($bets, $prediction);
-
-  $betsSerialize = serialize($prediction);
-  // save serialized data in a text file
-  file_put_contents('bets.txt', $betsSerialize);
-
-} else {
-
-  echo "You can't bet";
-
+    $m->execute([
+      'predictionId' => $predictionId,
+      'matchId' => $i,
+      'predictedResult' => $fighter,
+      'predictedRound' => $round
+    ]);
+  }  
 }
-
 ?>
