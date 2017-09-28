3e891b787ecc9bd2f4f012676c0dbbdec005f756
AA25 <aa25@live.ie>
2017-09-28 15:48:38 +0100

    continuing to do stuff on the prediction page

--- prediction.php
+++ prediction.php
@@ -10,6 +10,7 @@ if(isset($_SESSION['login'])){ ?>
           <link rel="stylesheet" type="text/css" href="css/bootstrap.min.css">
           <link rel="stylesheet" type="text/css" href="css/font-awesome-4.7.0/css/font-awesome.min.css"/>
           <link rel="stylesheet" type="text/css" href="css/jab6style.css"/>
+          <link href="https://fonts.googleapis.com/css?family=Allerta+Stencil" rel="stylesheet">
       </head>
       <body id="top">
         <?php include_once('includes/productHeader.inc.php'); 
@@ -45,73 +46,56 @@ if(isset($_SESSION['login'])){ ?>
               }
             }  
           ?>
-        <div class="predictionBanner">
-          <div class="container">
+        <div class="">
+          <div class="container predictionBanner">
+            <h4 class="cl-white txt-ctr">Event <?php echo $currentEvent ?></h4>
             
-            <form id="predictionForm" method="post" action="saveBet.php" class="marb-0 txt-ctr mar-auto w-55p">
-            <h4 class="cl-white txt-ctr">
-              Event 1
-            </h4>
-              <div class="match marb-15">
+            <!-- <form id="predictionForm" method="post" action="saveBet.php" class="marb-0 txt-ctr mar-auto w-55p"> -->
+            <form id="predictionForm" method="post" action="saveBet.php" class="marb-0 ">
+            <?php 
+              for($i = 0; $i < 2; $i++){
+                echo 
+                '<div class="match marb-15">
                   <div class="cl-white disp-fl">
                     <div>
-                      <p class="marb-0">
-                        Mcgregor
-                      </p>
+                      <p class="marb-0">'.$boxer1[$i].'</p>
                       <p class="pad-5">
-                        <input type="checkbox" id="Mcgregor" name="match1" value="McGregor"/>
-                        <label for="Mcgregor"><img class="fighters" src="images/fighters/blankFighter.png"/></label>
+                        <input type="checkbox" id="'.$boxer1[$i].'" name="match'.($i+1).'" value="'.$boxer1[$i].'"/>
+                        <label for="'.$boxer1[$i].'"><img class="fighters" src="images/fighters/blankFighter.png"/></label>
                       </p>
                     </div>
                     <div class="padt-10p">
-                      <input type="checkbox" id="draw1" name="match1" value="Mayweather"/>
-                      <label for="draw1"><img class="" src="images/DRAW1.png" style="height:80px; width:120px"/></label>
+                      <input type="checkbox" id="draw'.($i+1).'" name="match'.($i+1).'" value="Draw"/>
+                      <label for="draw'.($i+1).'"><img class="" src="images/DRAW1.png" style="height:80px; width:120px"/></label>
                     </div>
                     <div>
-                      <p class="marb-0">
-                        Mayweather
-                      </p>
+                      <p class="marb-0">'.$boxer2[$i].'</p>
                       <p class="pad-5">
-                        <input type="checkbox" id="Mayweather" name="match1" value="Mayweather"/>
-                        <label for="Mayweather"><img class="fighters" src="images/fighters/blankFighter.png" /></label>
+                        <input type="checkbox" id="'.$boxer2[$i].'" name="match'.($i+1).'" value="'.$boxer2[$i].'"/>                        
+                        <label for="'.$boxer2[$i].'"><img class="fighters" src="images/fighters/blankFighter.png"/></label>
                       </p>
-                    </div>
-                  </div>  
+                    </div> 
+                  </div> 
                   <div class="clearfix"></div>
-                  <div class="">
-                    <label for="match1Round" class="cl-white">Predict match to end on rounds or points : </label>
-                    <select name="match1Round">
-                      <option value="1">Round 1</option>
-                      <option value="2">Round 2</option>
-                      <option value="3">Round 3</option>
-                      <option value="4">Round 4</option>
-                      <option value="5">Round 5</option>
-                      <option value="6">Round 6</option>
-                      <option value="7">Round 7</option>
-                      <option value="8">Round 8</option>
-                      <option value="9">Round 9</option>
-                      <option value="10">Round 10</option>
-                      <option value="11">Round 11</option>
-                      <option value="12">Round 12</option>
-                      <option value="13">Points</option>
+                  <div><label for="match'.($i+1).'Round" class="cl-white">Predict match to end on rounds or points : </label>
+                    <select name="match'.($i+1).'Round">';
+                    for($rs = 1; $rs < 13; $rs++){
+                      echo '<option value="'.$rs.'">Round '.$rs.'</option>';
+                    }
+                    echo '<option value="13">Points</option>
                     </select>
                   </div>
-              </div>
-              
-              <div class="mart-10">
+                </div>'; };
+                echo '<div class="mart-10">
                 <label for="goldenGlove" class="cl-gold">Golden Glove Prediction : </label>
-                <select name="goldenGlove">
-                  <option value="0">McGregor vs Mayweather</option>
-                  <option value="1">Ward vs Jacobs</option>
-                  <option value="2">Golovkin vs Canelo</option>
-                  <option value="3">Joshua vs Fury</option>
-                  <option value="4">Groves vs Froch</option>
-                  <option value="5">Bellew vs Haye</option>
-                </select>
-              </div>
+                  <select name="goldenGlove">';
+                  for($i = 0; $i < count($boxer1); $i++){
+                    echo '<option value="'.$boxer1[$i].' vs '.$boxer2[$i].'">'.$boxer1[$i].' vs '.$boxer2[$i].'</option>';
+                  };
+              echo '</select></div>
+              <input type="submit" value="Submit Predictions" class="jab6Btn mar-10 fs-18 mart-30"/>';              
+            ?>
 
-              <input type="submit" value="Submit Predictions" class="jab6Btn mar-10 fs-18 mart-30" style=""/>
-            </form>
             <p class=" txt-ctr padl-20p padr-20p fs-14">
               The fight that will finish earliest out of the six selected matches. 
               Your Golden Glove prediction will be used if a tie breaker is needed. 
